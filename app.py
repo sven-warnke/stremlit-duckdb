fb85fbf54dc38fffc2ef6675436a3f711a4cc493
SvenWarnke <sven@ge-warnke.de>
2024-04-23 13:02:38 +0200

    bla

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pathlib as pl
 import duckdb
 import streamlit as st
 
+import plotly.express as px
+
 DATABASE = pl.Path("data.duckdb")
 
 
@@ -15,18 +17,20 @@ def main():
     st.title("DuckDB Streamlit Example")
 
     conn = get_db_connection()
+    conn.execute("CREATE TABLE IF NOT EXISTS data (a INTEGER, b VARCHAR)")
 
     st.write("## Table")
     st.write(conn.execute("SELECT * FROM data").fetchdf())
-
-    st.write("## Query")
-    query = st.text_area("Query", "SELECT * FROM data")
-    st.write(conn.execute(query).fetchdf())
 
     st.write("## SQL Editor")
     query = st.text_area("SQL Editor", "SELECT * FROM data")
     if st.button("Run"):
         st.write(conn.execute(query).fetchdf())
+        df = conn.execute("SELECT * FROM DATA").fetchdf()
+
+        st.plotly_chart(px.scatter(df, x='a', y='b'))
+
+
 
 
 if __name__ == "__main__":
