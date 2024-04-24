52ff0f6d83a82da8f80be5eb4529659c2a8cde9a
SvenWarnke <sven@ge-warnke.de>
2024-04-24 20:44:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,113 @@
+import datetime
+import io
 import pathlib as pl
+import time
 
 import duckdb
 import streamlit as st
-import datetime
 
 DATABASE = pl.Path("data/data.duckdb")
 
 
 @st.cache_resource
-def get_db_connection():
+def get_db_connection() -> duckdb.DuckDBPyConnection:
     return duckdb.connect(str(DATABASE))
 
 
-def handle_username(username: str) -> None:
+def handle_user_login(
+    username: str, camera_input: io.BytesIO, conn: duckdb.DuckDBPyConnection
+) -> None:
     if not username:
         st.error("Invalid username")
+
+    elif not camera_input:
+        st.error("Invalid camera input")
 
     else:
         st.session_state.username = username
 
+        query = "INSERT INTO users VALUES ($username, $image)"
+        conn.execute(
+            query,
+            parameters={
+                "username": st.session_state.username,
+                "image": camera_input.read(),
+            },
+        )
 
-def login() -> None:
-    st.text_input(
-        "Username",
-        key="input_username",
-        on_change=lambda: handle_username(st.session_state.input_username),
+
+def login(conn: duckdb.DuckDBPyConnection) -> None:
+    form = st.form(key="login_form")
+
+    with form:
+        st.text_input(
+            "Username",
+            key="input_username",
+        )
+        st.camera_input("Camera", key="camera_input")
+
+        st.form_submit_button(
+            "Login",
+            on_click=lambda: handle_user_login(
+                st.session_state.input_username,
+                st.session_state.camera_input,
+                conn=conn,
+            ),
+        )
+
+
+def insert_message(conn: duckdb.DuckDBPyConnection, message: str) -> None:
+    query = "INSERT INTO messages VALUES ($username, $message, $timestamp)"
+    timestamp = datetime.datetime.now()
+    conn.execute(
+        query,
+        parameters={
+            "username": st.session_state.username,
+            "message": message,
+            "timestamp": timestamp,
+        },
     )
 
 
-def chat() -> None:
+def chat(conn: duckdb.DuckDBPyConnection) -> None:
+    st.write("## Chat")
+
+    st.chat_input(
+        "message",
+        key="message",
+        on_submit=lambda: insert_message(conn, st.session_state.message),
+    )
+
+    message_container = st.container(height=300)
+
+    df = (
+        conn.execute("""
+        SELECT * FROM messages AS m JOIN users AS u on m.username = u.username ORDER BY timestamp DESC LIMIT 10
+        """)
+        .fetchdf()
+        .sort_values(by="timestamp", ascending=True)
+    )
+    for _, line in df.iterrows():
+        message_container.chat_message(
+            line["username"], avatar=io.BytesIO(line["image"])
+        ).write(line["message"])
+
+
+def main():
     conn = get_db_connection()
     conn.execute(
         "CREATE TABLE IF NOT EXISTS messages (username VARCHAR, message VARCHAR, timestamp TIMESTAMP)"
     )
 
-    st.write("## Chat")
-
-    message = st.chat_input("message")
-    timestamp = datetime.datetime.now()
-
-    message_container = st.container(height=300)
-
-    if message:
-        query = "INSERT INTO messages VALUES ($username, $message, $timestamp)"
-        conn.execute(
-            query,
-            parameters={
-                "username": st.session_state.username,
-                "message": message,
-                "timestamp": timestamp,
-            },
-        ).fetchdf()
-        df = conn.execute("SELECT * FROM messages").fetchdf()
-        for _, line in df.iterrows():
-            message_container.chat_message(line["username"]).write(line["message"])
-
-
-def main():
+    conn.execute("CREATE TABLE IF NOT EXISTS users (username VARCHAR, image BLOB)")
     st.title("DuckDB Streamlit Example")
 
     if "username" not in st.session_state or not st.session_state.username:
-        login()
+        login(conn)
     else:
-        chat()
+        chat(conn)
+        time.sleep(10)
+        st.rerun()
 
 
 if __name__ == "__main__":
